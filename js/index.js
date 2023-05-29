b4af208e1e8c2249e3428e3cf5c0a054337e1536
prathap <prathapy67@gmail.com>
2023-05-29 18:51:53 +0530

    updates

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 window.addEventListener('DOMContentLoaded', () => {
     const heroEl = document.getElementById("hero");
+    const membersEl = document.getElementById("members");
+    const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
     window.onscroll = function () {
         // setTimeout(() => {
         //     console.log('>> window.pageYOffset',document.body.scrollTop,  document.documentElement.scrollTop)
@@ -11,11 +14,30 @@ window.addEventListener('DOMContentLoaded', () => {
         //   }, 1000);
 
         // console.log('>> window.pageYOffset', document.body.scrollTop, document.documentElement.scrollTop)
-        
+
         if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-            heroEl.setAttribute('class','hide-hero');
+            heroEl.setAttribute('class', 'hide-hero');
+            membersEl.style.paddingTop = '185px';
         } else {
-            heroEl.setAttribute('class','show-hero');
+            heroEl.setAttribute('class', 'show-hero');
+            membersEl.style.paddingTop = '0';
+        }
+
+
+        // SCROLL TO TOP
+        // When the user scrolls down 20px from the top of the document, show the button
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+            scrollToTopBtn.style.display = "block";
+        } else {
+            scrollToTopBtn.style.display = "none";
         }
     }
+
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
+    scrollToTopBtn.onclick = topFunction;
 })
